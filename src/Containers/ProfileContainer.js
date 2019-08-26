d20b22662a8be534c4794bad54734305e5be9aa6
github username <dallasbille@gmail.com>
2019-08-26 09:47:46 -0400

    need to pull from master

--- src/Containers/ProfileContainer.js
+++ src/Containers/ProfileContainer.js
@@ -1,44 +1,48 @@
-import React from 'react'
-import UserDonations from '../Components/ProfileComponents/UserDonations'
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemText from '@material-ui/core/ListItemText';
-import ListItemAvatar from '@material-ui/core/ListItemAvatar';
-import Avatar from '@material-ui/core/Avatar';
-import Typography from '@material-ui/core/Typography';
-import { connect } from 'react-redux'
-import { fetchAdventures } from '../Adapters/adventureAdapters'
-import UserAdventureList from '../Components/ProfileComponents/UserAdventureList'
-import ProfileDonationsContainer from '../Containers/ProfileDonationsContainer'
-import UserInfo from '../Components/ProfileComponents/UserInfo'
-
+import React from "react";
+import UserDonations from "../Components/ProfileComponents/UserDonations";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
+import ListItemAvatar from "@material-ui/core/ListItemAvatar";
+import Avatar from "@material-ui/core/Avatar";
+import Typography from "@material-ui/core/Typography";
+import { connect } from "react-redux";
+import { fetchAdventures } from "../Adapters/adventureAdapters";
+import UserAdventureList from "../Components/ProfileComponents/UserAdventureList";
+import ProfileDonationsContainer from "../Containers/ProfileDonationsContainer";
+import UserInfo from "../Components/ProfileComponents/UserInfo";
 
 class ProfileContainer extends React.Component {
-
-     loadUserAdventures = () => {
-        const userAdventures = this.props.adventures.adventures.filter(adventure => {
-            return adventure.user_id === this.props.user.id
-        })
-        return userAdventures.map(adventure => {
-            return <UserAdventureList myAdventure={adventure}/>
-        })
+  loadUserAdventures = () => {
+    const userAdventures = this.props.adventures.adventures.filter(
+      adventure => {
+        return adventure.user_id === this.props.user.id;
+      }
+    );
+    if (userAdventures.length > 0) {
+      return userAdventures.map(adventure => {
+        return <UserAdventureList myAdventure={adventure} />;
+      });
     }
+  };
 
-    render(){
-    return(
-        <div>
-          <UserInfo/>
-          <ProfileDonationsContainer donations={this.props.user.user_donations}/>
-          {this.loadUserAdventures()}
-        </div>
-
-    )
-    }
+  render() {
+    console.log(this.props.adventures.adventures);
+    return (
+      <div>
+        <UserInfo />
+        <ProfileDonationsContainer donations={this.props.user.user_donations} />
+        {this.loadUserAdventures()}
+      </div>
+    );
+  }
 }
 
+const mapStateToProps = state => {
+  return state;
+};
 
-const mapStateToProps  = (state) => {
-    return state
-}
-
-export default connect(mapStateToProps, {fetchAdventures})(ProfileContainer)
+export default connect(
+  mapStateToProps,
+  { fetchAdventures }
+)(ProfileContainer);
